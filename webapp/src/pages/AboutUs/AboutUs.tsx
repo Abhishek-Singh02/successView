a20a67f41b74d155ddc219171a399c5af0152d55
Abhishek-Singh02 <abhishek9502as@gmail.com>
2022-12-31 14:40:40 +0530

    style: update about is text

--- webapp/src/pages/AboutUs/AboutUs.tsx
+++ webapp/src/pages/AboutUs/AboutUs.tsx
@@ -9,10 +9,10 @@ export const AboutUs: FC<AboutUsProps> = () => {
         <Flex center>
             <Flex width="full" padding="9" direction="column" gap="5">
                 <Text variant="heading6">
-                    At our company, we are committed to providing innovative
-                    solutions and exceptional service to our clients. We believe
-                    that by working together and leveraging the unique skills
-                    and expertise of our team, we can achieve great things.
+                    Welcome to Truly Succeed Magazine! We are a dynamic and
+                    dedicated team of writers, editors, and designers committed
+                    to delivering top-quality content and exceptional service to
+                    our readers and advertisers.
                 </Text>
                 <Text variant="heading6">
                     Our team is made up of experienced professionals who are
@@ -21,22 +21,40 @@ export const AboutUs: FC<AboutUsProps> = () => {
                     together to deliver the best possible results.
                 </Text>
                 <Text variant="heading6">
-                    We are constantly seeking out new opportunities and
-                    challenges, and we are not afraid to take risks in order to
-                    push the boundaries of what is possible. We are dedicated to
-                    staying at the forefront of our industry, and we are always
-                    looking for ways to improve and evolve.
+                    At Truly Succeed, we understand the importance of staying
+                    informed and up-to-date on the latest trends and
+                    developments. That's why we work tirelessly to bring our
+                    readers engaging and informative articles on a wide range of
+                    topics, including business, finance, current events, and
+                    more. Our goal is to provide a well-rounded and engaging
+                    reading experience that keeps our readers coming back for
+                    more.
                 </Text>
                 <Text variant="heading6">
-                    We take pride in our work and in the relationships we have
-                    built with our clients. We believe in building partnerships
-                    based on trust and mutual respect, and we are committed to
-                    delivering the highest level of service and support.
+                    But we don't just stop at delivering great content to our
+                    readers. We also understand the importance of building
+                    strong relationships with our advertisers and partners.
+                    That's why we offer a range of advertising options and can
+                    work with you to develop a customized marketing plan that
+                    meets your needs and budget.
                 </Text>
                 <Text variant="heading6">
-                    Thank you for considering us for your next project. We look
-                    forward to the opportunity to work with you and to help
-                    bring your vision to life.
+                    Our dedicated team is here to help you reach your target
+                    audience and grow your business. In addition to our
+                    advertising services, we also offer promotional
+                    opportunities through our social media channels and email
+                    newsletters. We are proud of the high-quality product that
+                    we deliver to our readers and advertisers, and are committed
+                    to continuously improving and evolving to meet the needs of
+                    our audience.
+                </Text>
+                <Text variant="heading6">
+                    We take pride in the long-term relationships we have built
+                    with our readers and partners, and strive to make a positive
+                    impact on the lives of those we reach through our magazine.
+                    Thank you for choosing Truly Succeed Magazine. We look
+                    forward to working with you and helping you achieve your
+                    business goals.
                 </Text>
             </Flex>
             <Flex css={{ "@mbp1": { display: "none" } }}>
