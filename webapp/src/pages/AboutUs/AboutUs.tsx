8c9aacb5db8455ca188219d6518b010707df788c
Abhishek-Singh02 <abhishek9502as@gmail.com>
2023-01-19 20:14:28 +0530

    chore: update about us

--- webapp/src/pages/AboutUs/AboutUs.tsx
+++ webapp/src/pages/AboutUs/AboutUs.tsx
@@ -9,42 +9,32 @@ export const AboutUs: FC<AboutUsProps> = () => {
         <Flex center>
             <Flex width="full" padding="9" direction="column" gap="7">
                 <Text variant="heading6">
-                    Welcome to Truly Succeed Magazine! We are a dynamic and
-                    dedicated team of writers, editors, and designers committed
-                    to delivering top-quality content and exceptional service to
-                    our readers and advertisers.
+                    Truly Succeed is a media publication that gives information
+                    about technology and business news. It's for people at the
+                    highest levels of business, so they can stay up-to-date on
+                    the latest news and trends. It helps them make better
+                    decisions and be successful in their business world.
                 </Text>
                 <Text variant="heading6">
-                    Our team is made up of experienced professionals who are
-                    passionate about what they do. We have engineers, designers,
-                    business development specialists, and more, all working
-                    together to deliver the best possible results.
+                    Truly Succeed offers business leaders, managers, and HR
+                    personnel the chance to showcase their innovative strategies
+                    and uncover useful solutions to help their businesses
+                    thrive. With this platform, you can learn how to be better
+                    at what you do, and ultimately, reach success in growing
+                    your business.
                 </Text>
                 <Text variant="heading6">
-                    At Truly Succeed, we understand the importance of staying
-                    informed and up-to-date on the latest trends and
-                    developments. That's why we work tirelessly to bring our
-                    readers engaging and informative articles on a wide range of
-                    topics, including business, finance, current events, and
-                    more. Our goal is to provide a well-rounded and engaging
-                    reading experience that keeps our readers coming back for
-                    more.
+                    Truly Succeed is a website that helps businesses stay up to
+                    date with new products, services, and technology. They
+                    provide businesses with important information so they can be
+                    successful.
                 </Text>
                 <Text variant="heading6">
-                    But we don't just stop at delivering great content to our
-                    readers. We also understand the importance of building
-                    strong relationships with our advertisers and partners.
-                    That's why we offer a range of advertising options and can
-                    work with you to develop a customized marketing plan that
-                    meets your needs and budget.
-                </Text>
-                <Text variant="heading6">
-                    We take pride in the long-term relationships we have built
-                    with our readers and partners, and strive to make a positive
-                    impact on the lives of those we reach through our magazine.
-                    Thank you for choosing Truly Succeed Magazine. We look
-                    forward to working with you and helping you achieve your
-                    business goals.
+                    We want to make a place for successful people to talk and
+                    tell their stories so that younger people can learn from
+                    them. We want the young people to be able to use what they
+                    learn to make better businesses for customers and become
+                    leaders in the future.
                 </Text>
             </Flex>
             <Flex css={{ "@mbp1": { display: "none" }, mr: "$9" }}>
